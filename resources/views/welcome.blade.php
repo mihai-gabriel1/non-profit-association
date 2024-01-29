00047a366549450630965a33212d1b746ce07394
Mihai Gabriel <mr.eu21@gmail.com>
2024-01-30 00:06:08 +0200

    header created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,68 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+        <title>Asociatia Islaz 21 | Daruieste Speranta</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
         @vite(['resources/js/app.js'])
-
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
     <body>
-    Testing this <button class="butonas">butonel</button>
+    <div class="header">
+        <nav class="navbar navbar-dark navbar-expand-sm bg-dark fixed-top">
+        <div class="container">
+            <a href="/" class="navbar-brand">
+                <i class="fas fa-blog"></i> &nbsp;
+                Asociația Islaz 21
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Acasǎ
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Despre noi
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Blog
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        <div class="header-title">
+            <p class="give-hand">Întinde o mânā</p>
+            <p class="better-world">Pentru o lume mai bunā</p>
+        </div>
+        <div class="header-buttons">
+            <p class="see-causes">
+                Cauzele noastre
+            </p>
+            <p class="find-more">
+                Aflā mai multe
+            </p>
+        </div>
+    </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     </body>
 </html>
