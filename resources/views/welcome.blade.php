d7ce5f6d26ec83a15c3e2666f21a56155ee751c6
Mihai Gabriel <mr.eu21@gmail.com>
2024-02-01 23:00:42 +0200

    team section refactor, carousel added.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="owl-carousel owl-theme">
-        <div class="item"><h4>1</h4></div>
-        <div class="item"><h4>2</h4></div>
-        <div class="item"><h4>3</h4></div>
-        <div class="item"><h4>4</h4></div>
-        <div class="item"><h4>5</h4></div>
-        <div class="item"><h4>6</h4></div>
-        <div class="item"><h4>7</h4></div>
-        <div class="item"><h4>8</h4></div>
-        <div class="item"><h4>9</h4></div>
-        <div class="item"><h4>10</h4></div>
-        <div class="item"><h4>11</h4></div>
-        <div class="item"><h4>12</h4></div>
-    </div>
     <div class="header">
         <nav class="navbar navbar-dark navbar-expand-sm bg-dark">
             <div class="container">
@@ -311,146 +297,166 @@
                 Hai sa facem cunoştințǎ
             </p>
             <div class="row team-inner">
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/raul.jpeg') }}"
-                                 alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Raul Gheorghina
-                            </p>
-                            <p class="team-member-title">
-                                Preşedinte
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/ali.jpeg') }}" alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Ali Cranta
-                            </p>
-                            <p class="team-member-title">
-                                Membru
-                            </p>
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/raul.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Raul Gheorghina
+                                    </p>
+                                    <p class="team-member-title">
+                                        Preşedinte
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/valentin.jpeg') }}"
-                                 alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Valentin Uba
-                            </p>
-                            <p class="team-member-title">
-                                Membru
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/cornelia.jpeg') }}"
-                                 alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Cornelia Popescu
-                            </p>
-                            <p class="team-member-title">
-                                Membru
-                            </p>
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/ali.jpeg') }}" alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Ali Cranta
+                                    </p>
+                                    <p class="team-member-title">
+                                        Membru
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/cristina.jpeg') }}"
-                                 alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Cristina Gabriela
-                            </p>
-                            <p class="team-member-title">
-                                Membru
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/dana.jpeg') }}"
-                                 alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Dana Iordan
-                            </p>
-                            <p class="team-member-title">
-                                Membru
-                            </p>
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/valentin.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Valentin Uba
+                                    </p>
+                                    <p class="team-member-title">
+                                        Membru
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/nineta.jpeg') }}"
-                                 alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Nineta Cranta
-                            </p>
-                            <p class="team-member-title">
-                                Membru
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/adelina.jpeg') }}"
-                                 alt="team member">
-                        </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Adelina Naidin
-                            </p>
-                            <p class="team-member-title">
-                                Voluntar
-                            </p>
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/cornelia.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Cornelia Popescu
+                                    </p>
+                                    <p class="team-member-title">
+                                        Membru
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="team_item">
-                        <div class="team_img">
-                            <img class="card-img-top img-fluid" src="{{ asset('assets/team/luiza.jpeg') }}"
-                                 alt="team member">
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/cristina.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Cristina Gabriela
+                                    </p>
+                                    <p class="team-member-title">
+                                        Membru
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="team_name">
-                            <p class="team-member-name">
-                                Luiza Ciucu
-                            </p>
-                            <p class="team-member-title">
-                                Voluntar
-                            </p>
+                    </div>
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/dana.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Dana Iordan
+                                    </p>
+                                    <p class="team-member-title">
+                                        Membru
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/nineta.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Nineta Cranta
+                                    </p>
+                                    <p class="team-member-title">
+                                        Membru
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/adelina.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Adelina Naidin
+                                    </p>
+                                    <p class="team-member-title">
+                                        Voluntar
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="">
+                            <div class="team_item">
+                                <div class="team_img">
+                                    <img class="card-img-top img-fluid" src="{{ asset('assets/team/luiza.jpeg') }}"
+                                         alt="team member">
+                                </div>
+                                <div class="team_name">
+                                    <p class="team-member-name">
+                                        Luiza Ciucu
+                                    </p>
+                                    <p class="team-member-title">
+                                        Voluntar
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -542,19 +548,26 @@
         <script>
             $('.owl-carousel').owlCarousel({
                 loop:true,
-                margin:10,
+                margin:50,
                 responsiveClass:true,
+                autoplay: true,
+                touchDrag: true,
                 responsive:{
                     0:{
                         items:1,
                         nav:true
                     },
+                    350:{
+                        items:1,
+                        nav:true
+                    },
                     600:{
-                        items:3,
-                        nav:false
+                        items:2,
+                        nav:false,
+                        margin: 10,
                     },
                     1000:{
-                        items:5,
+                        items:3,
                         nav:true,
                         loop:false
                     }
