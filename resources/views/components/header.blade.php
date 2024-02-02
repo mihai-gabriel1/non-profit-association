fcf012801bd138243d71f88c8bc01f28df29a36c
Mihai Gabriel <mr.eu21@gmail.com>
2024-02-02 23:48:59 +0200

    structured it into reusable components

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,60 @@
+<div class="header">
+    <nav class="navbar navbar-dark navbar-expand-md bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">
+                <i class="fas fa-blog"></i> &nbsp;
+                Asociația Islaz 21
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Acasǎ
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Despre noi
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Proiecte
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Vino cu noi
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Blog
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="header-title">
+        <p class="give-hand">Întinde o mânā</p>
+        <p class="better-world">Pentru o lume mai bunā</p>
+    </div>
+    <div class="header-buttons">
+        <p class="see-causes">
+            Cauzele noastre
+        </p>
+        <p class="find-more">
+            Aflā mai multe
+        </p>
+    </div>
+</div>
